e66eca5b681627ba0285a01c4b26611eab1ee58b
Cory <coryodonoghue1@gmail.com>
2020-10-22 11:10:04 +0100

    second commit

--- src/components/content.js
+++ src/components/content.js
@@ -5,7 +5,7 @@ export class Content extends React.Component {//extending react class Component
     render() {
         return (
             <div>
-                <h1> Hello Bitch!</h1>
+                <h1> Hello World!</h1>
                 <h2>It is {new Date().toLocaleTimeString()}.</h2>
                 {/* //Current time will be presented on Content page */}
             </div>
